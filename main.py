6be4b310c18f21fcda0116df417b8e2b3098ffc3
SaucyPigeon <emilianb@hotmail.co.uk>
2020-07-17 23:03:38 +0100

    main, task_manager

---
+++ main.py
@@ -0,0 +1,62 @@
+# Old School Helper
+# main.py
+# Allows user to add slayer tasks.
+# Slayer tasks have preferred gear setups saved as well.
+
+import task_manager
+
+class command:
+    def __init__(self, name, desc, func):
+        self.name = name
+        self.desc = desc
+        self.func = func
+
+    def __str__(self):
+        return self.name + ": " + self.desc
+
+def quote(message, quote_s):
+    return quote_s + message + quote_s
+
+def get_command_with_name(commands, name):
+    for command in commands:
+        if command.name == name:
+            return command
+    return None
+
+
+
+def entry():
+    print(quote("Old School Helper", "==="))
+    print("Available commands:")
+
+    tasks = task_manager.task_manager()
+    
+    commands = [
+        command("add task", "add new task", tasks.add_new_task),
+        command("select", "select task", tasks.select_task),
+        command("deselect", "deselect task", tasks.deselect_task),
+        command("delete", "delete selected task", tasks.delete_task),
+        command("add gear", "add gear to task", tasks.add_gear_to_task),
+        command("remove gear", "remove gear from task", tasks.remove_gear_from_task),
+        command("display gear", "display task gear", tasks.display_task_gear),
+        command("help", "display available commands", None)
+        ]
+
+    for c in commands:
+        print(str(c))
+
+    while True:
+        print("Enter command:")
+        command_input = input()
+        selected_command = get_command_with_name(commands, command_input)
+        if selected_command is None:
+            print("That is not a valid command. Please try again.")
+            continue
+        if selected_command.name == "help":
+            for c in commands:
+                print(str(c))
+            continue
+        
+        selected_command.func()
+
+entry()
---
+++ task_manager.py
@@ -0,0 +1,91 @@
+# Old School Helper
+# task_manager.py
+# Encapsulates program state
+
+def input_bool(message):
+    print(message)
+    i = input()
+    if i == "y":
+        return True
+    if i == "n":
+        return False
+    print("Invalid input: has to be either \"y\" or \"n\". Please try again.")
+    return input_bool(message)
+
+class task:
+    def __init__(self, monster):
+        self.monster = monster
+        self.gear = None
+
+class task_manager:
+    def __init__(self):
+        self.selected_task = None
+        self.tasks = []
+
+
+    def add_new_task(self):
+        print("Enter task monster:")
+        monster = input()
+        t = task(monster)
+        self.selected_task = t
+        self.tasks.append(t)
+        print(monster + " slayer task added and selected.")
+
+    def select_task(self):
+        return None
+    
+    def deselect_task(self):
+        return None
+    
+    def delete_task(self):
+        return None
+    
+    def add_gear_to_task(self):
+        if self.selected_task is None:
+            print("No current task selected.")
+            return
+        if not self.selected_task.gear is None:
+            print("Gear already exists for this task (" + self.selected_task.monster + ").")
+            while True:
+                if not input_bool("Are you sure that you want to overwrite existing gear?"):
+                    return
+
+        self.selected_task.gear = {}
+        slots = [
+            "helmet",
+            "cape",
+            "amulet",
+            "ammo",
+            "weapon",
+            "offhand",
+            "torso",
+            "legs",
+            "gloves",
+            "boots",
+            "ring"
+            ]
+        two_handed = False
+        for slot in slots:
+            if slot == "weapon":
+                two_handed = input_bool("Is the weapon two-handed?")
+            if two_handed and slot == "offhand":
+                continue
+            print("Enter item for " + slot + " slot:")
+            i = input()
+            self.selected_task.gear[slot] = i
+        print("Gear successfully added for " + self.selected_task.monster + ".")
+            
+    
+    def remove_gear_from_task(self):
+        return None
+
+    def display_task_gear(self):
+        if self.selected_task is None:
+            print("No current task selected.")
+            return
+        if self.selected_task.gear is None:
+            print("Slayer task has no gear recorded.")
+            return
+        print("Gear for " + self.selected_task.monster + ":")
+        for key, value in self.selected_task.gear.items():
+            print(key + " slot: " + value)
